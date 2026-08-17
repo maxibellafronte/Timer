600e8597a3e720c1620d2970b13ec69feac3c5b4
maxibellafronte <maxil.bellafronte@gmail.com>
2026-08-17 00:59:32 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,53 @@
+// Service Worker para MIX WOD - Temporizador
+// Permite que la app funcione offline una vez instalada en el iPhone
+
+const CACHE_NAME = "mixwod-cache-v2";
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png"
+  // Si tu index.html carga CSS o JS en archivos separados
+  // (ej: "./style.css", "./app.js"), agregalos a esta lista.
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request)
+          .then((response) => {
+            const responseClone = response.clone();
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, responseClone);
+            });
+            return response;
+          })
+          .catch(() => cached)
+      );
+    })
+  );
+});
